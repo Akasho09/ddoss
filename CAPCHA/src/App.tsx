b5d0a02b10b2d06e2f4e6db4277c422a549441e5
Akasho09 <aakashbwd@gmail.com>
2025-04-10 11:46:52 +0530

    neww

--- CAPCHA/src/App.tsx
+++ CAPCHA/src/App.tsx
@@ -1,29 +1,29 @@
-// import { Turnstile } from '@marsidev/react-turnstile'
+import { Turnstile } from '@marsidev/react-turnstile'
 
 import './App.css'
-// import axios from 'axios'
-// import { useState } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 function App() {
-  // const [token, setToken] = useState<string>("")
+  const [token, setToken] = useState<string>("")
 
   return (
     <>
       <input placeholder='OTP'></input>
       <input placeholder='New password'></input>
-{/* 
+
       <Turnstile onSuccess={(token) => {
         setToken(token)
-      }} siteKey='0x4AAAAAAAXtEe2JIeAEUcjX' >
-      akash</Turnstile> */}
+      }} siteKey='0x4AAAAAABIAKmQiaS3Mlm4b' >
+      akash</Turnstile>
 
-      {/* <button onClick={() => {
+      <button onClick={() => {
         axios.post("http://localhost:3000/reset-password", {
           email: "harkirat@gmail.com",
           otp: "123456",
           token: token,
         })
-      }}>Update password</button> */}
+      }}>Update password</button>
     </>
   )
 }
